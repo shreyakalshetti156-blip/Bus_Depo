d30858cf7e51125caba0063ff5a2fff454c6738c
shreyakalshetti156-blip <shreyakalshetti156@gmail.com>
2026-07-24 21:05:50 +0530

    Add implementation code

--- src/main/java/com/Bus/utility/DBConnection.java
+++ src/main/java/com/Bus/utility/DBConnection.java
@@ -11,8 +11,7 @@ public class DBConnection {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/Bus_Depot", "root", "tiger");
 		} catch (ClassNotFoundException | SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+				e.printStackTrace();
 		}
 		return con;
 	}
--- src/main/java/com/busdepot/model/Bus.java
+++ src/main/java/com/busdepot/model/Bus.java
@@ -17,7 +17,6 @@ public class Bus {
         this.depotId = depotId;
     }
 
-<<<<<<< HEAD
     public Integer getBusId() { 
     	return busId; 
     	}
@@ -30,18 +29,28 @@ public class Bus {
     public void setBusNumber(String busNumber) { 
     	this.busNumber = busNumber; 
     	}
-=======
-    public Integer getBusId() { return busId; }
-    public void setBusId(Integer busId) { this.busId = busId; }
-    public String getBusNumber() { return busNumber; }
-    public void setBusNumber(String busNumber) { this.busNumber = busNumber; }
->>>>>>> 543e668ada4b0481b99425993b713150c4bc0146
-    public String getBusType() { return busType; }
-    public void setBusType(String busType) { this.busType = busType; }
-    public Integer getCapacity() { return capacity; }
-    public void setCapacity(Integer capacity) { this.capacity = capacity; }
-    public Integer getDepotId() { return depotId; }
-    public void setDepotId(Integer depotId) { this.depotId = depotId; }
-    public String getDepotName() { return depotName; }
-    public void setDepotName(String depotName) { this.depotName = depotName; }
+    public String getBusType() {
+    	return busType; 
+    	}
+    public void setBusType(String busType) { 
+    	this.busType = busType;
+    	}
+    public Integer getCapacity() {
+    	return capacity;
+    	}
+    public void setCapacity(Integer capacity) {
+    	this.capacity = capacity;
+    	}
+    public Integer getDepotId() {
+    	return depotId;
+    	}
+    public void setDepotId(Integer depotId) {
+    	this.depotId = depotId; 
+    	}
+    public String getDepotName() {
+    	return depotName;
+    	}
+    public void setDepotName(String depotName) {
+    	this.depotName = depotName; 
+    	}
 }
