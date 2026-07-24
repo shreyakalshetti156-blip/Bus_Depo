45f58a8c6215947e6049cb4a4b6d19d7ee1ad566
shreya kalshetty <shreyakalshetti156@gmail.com>
2026-07-24 19:38:41 +0530

    Add files via upload

---
+++ build/classes/com/dcl/Bus/utility/DBConnection.java
@@ -0,0 +1,21 @@
+package com.Bus.utility;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+	public static Connection requestConnection() {
+		Connection con=null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/Bus_Depot", "root", "tiger");
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return con;
+	}
+}
+
+
